5aca06938934c0f3964c4e566961bc6e71b04ac4
Bubblesphere <dallaire.deric@gmail.com>
2020-11-13 20:05:24 -0500

    Moved lazyLoadSrc to images object

--- example/pages/index.js
+++ example/pages/index.js
@@ -9,15 +9,8 @@ export default () => (
             images={[
                 {
                     src:
-                        'https://m.media-amazon.com/images/G/01/digital/music/merch/2019/LandingPages/katana/subtest/Katana_Aquisition_Desktop_1_1x.jpg',
-                    caption: 'Windows 10 Dark Mode Setting',
-                    alt: 'Windows 10 Dark Mode Setting',
-                    width: 2848,
-                    height: 2035
-                },
-                {
-                    src:
                         'https://timellenberger.com/static/blog-content/dark-mode/macos-dark-mode.png',
+
                     caption: 'macOS Mojave Dark Mode Setting',
                     alt: 'macOS Mojave Dark Mode Setting',
                     width: 1200,
@@ -25,7 +18,22 @@ export default () => (
                 },
                 {
                     src:
+                        'https://m.media-amazon.com/images/G/01/digital/music/merch/2019/LandingPages/katana/subtest/Katana_Aquisition_Desktop_1_1x.jpg',
+
+                    lazyLoadSrc:
+                        'https://img.icons8.com/pastel-glyph/2x/search--v2.png',
+
+                    caption: 'Windows 10 Dark Mode Setting',
+                    alt: 'Windows 10 Dark Mode Setting',
+                    width: 2848,
+                    height: 2035
+                },
+
+                {
+                    src:
                         'https://timellenberger.com/static/blog-content/dark-mode/android-9-dark-mode.jpg',
+                    lazyLoadSrc:
+                        'https://www.clker.com/cliparts/3/m/v/Y/E/V/small-red-apple-hi.png',
                     caption: 'Android 9.0 Dark Mode Setting',
                     alt: 'Android 9.0 Dark Mode Setting',
                     width: 1280,
